660ab01f93c1f5a645df8da10cfce8331ad6cc35
AdrielBar <adrielbarzola@ufl.edu>
2024-07-09 22:31:41 -0400

    Update app.jscommented out mongo block

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const mongoose = require ('mongoose');
 
 const app = express();
 
-const dbURI = "mongodb://localhost:27017/lib-manage"
-mongoose.connect(dbURI)
-    .then((result) => app.listen(8000))
-    .catch((err) => console.log(err));
+// Uncomment this line once you have mongodb installed, running on its default port, and have a databse called lib-manage 
+// const dbURI = "mongodb://localhost:27017/lib-manage"
+// mongoose.connect(dbURI)
+//     .then((result) => app.listen(8000))
+//     .catch((err) => console.log(err));
+app.listen(8000);
 
 app.set('view engine', 'ejs');
 
