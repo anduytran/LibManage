8972f449f4f27639190ed15bdeee536b6bd88ae2
AdrielBar <adrielbarzola@ufl.edu>
2024-08-05 05:55:01 -0400

    got new user creation working, basic cookies working

---
+++ controllers/authController.js
@@ -0,0 +1,64 @@
+const User = require("../models/user");
+const jwt = require('jsonwebtoken')
+
+
+const handleErrors = (err) => {
+        console.log(err.message, err.code);
+        let errors = { email: '', password: '' };
+    
+        // duplicate email error
+        if (err.code === 11000) {
+        errors.email = 'that email is already registered';
+        return errors;
+        }
+    
+        // validation errors
+        if (err.message.includes('user validation failed')) {
+            Object.values(err.errors).forEach(({ properties }) => {
+                errors[properties.path] = properties.message;
+            });
+        }
+    
+        return errors;
+}
+// PRETTY PLEASE, GET THIS SECRET STRING OUT OF THERE BEFORE ANYONE SEES THIS
+const createToken = (id) => {
+    return jwt.sign({ id }, 'lib-manage secret', {
+        expiresIn: 1800
+    });
+}
+
+module.exports.signup_get = (req, res) => {
+    res.render('signup');
+}
+
+module.exports.login_get = (req, res) => {
+    res.render('login');
+}
+
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+        const user = await User.create({ email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            maxAge: 1800 * 1000
+        });
+        res.status(201).json({ user:user._id });
+    }
+    catch(err) {
+      const errors = handleErrors(err);
+      res.status(400).json({ errors });
+    }
+   
+}
+
+module.exports.login_post = async (req, res) => {
+    const { email, password } = req.body;
+  
+    console.log(email, password);
+    res.send('user login');
+}
+
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
+});
+// fire a function after doc is saved to db
+userSchema.post('save',  function (doc, next) { 
+
+
+    next();
+})
+
+// fire a function before doc is saved to db
+userSchema.pre('save', async function(next) { 
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    
+    next();
+})
+
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
