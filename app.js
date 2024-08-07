ab2484bf4d9f12c81449e64816360011fe883714
AdrielBar <adrielbarzola@ufl.edu>
2024-08-06 22:35:02 -0400

    added handler for control center

--- app.js
+++ app.js
@@ -162,7 +162,13 @@ app.post('/add-event', (req, res) => {
 });
 app.use(authRoutes);
 
+app.get('/checkout', (req,res) => {
+    res.redirect();
+})
 
+app.get('/control-center', (req,res) => {
+    res.render('controlCenter');
+})
 
 app.get('/employees', requireAuth, (req, res) => res.render('employees'))
 
